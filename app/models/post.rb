7e97faa30af85f89830142899009b51ee71c83fa
Suzu <ec2-user@ip-172-31-44-241.us-east-2.compute.internal>
2020-10-30 12:49:39 +0000

    commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
     validates(:title, 
-    presence:({ message: "この項目は必須です"}),
-    length:({ minimum: 3 , message: "タイトルは３文字以上で入力してください"}))
+    presence:{ message: "この項目は必須です"},
+    length:{ minimum: 3 , message: "タイトルは３文字以上で入力してください"})
     
     validates :body,
     presence: { message: "この項目は必須です"}
