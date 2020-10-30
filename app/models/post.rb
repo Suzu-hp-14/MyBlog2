050698da68ac1bae3366fc5306b4f4c1bdd2b90c
EC2 Default User <ec2-user@ip-172-31-44-241.us-east-2.compute.internal>
2020-10-30 07:40:58 +0000

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+    validates(:title, 
+    presence:({ message: "この項目は必須です"}),
+    length:({ minimum: 3 , message: "タイトルは３文字以上で入力してください"}))
+    
+    validates :body,
+    presence: { message: "この項目は必須です"}
+end
