258af22538f68af4caf3c14aae1ebfbb447358af
Suzu <ec2-user@ip-172-31-44-241.us-east-2.compute.internal>
2020-11-01 14:34:04 +0000

    編集削除実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,14 +10,33 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
-    
+        
     def create
         @post = Post.new(posts_params)
-        if @post.save
+        if @post.save(posts_params)
             redirect_to posts_path
         else
             render 'new'
         end
+    end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(posts_params)
+            redirect_to posts_path
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
     end
     
     private
