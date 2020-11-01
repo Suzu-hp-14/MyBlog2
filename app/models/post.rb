ae5b507c0c1d39bc114eb37c511d72f04bafbfe5
Suzu <ec2-user@ip-172-31-44-241.us-east-2.compute.internal>
2020-11-01 16:19:22 +0000

    コメント機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    has_many :comments, dependent: :destroy
     validates(:title, 
     presence:{ message: "この項目は必須です"},
     length:{ minimum: 3 , message: "タイトルは３文字以上で入力してください"})
